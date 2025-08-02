dbc840bedf4609a5c56e1040cbbf0586f7f082c2
Sleepy Mole <xiayjchn@gmail.com>
2025-08-02 12:40:24 +0800

    Upgrade Corplink to latest (#2)

--- Makefile
+++ Makefile
@@ -1,2 +1,28 @@
+.PHONY: build push run
+
+REPO := sleepymole/corplink
+DATE := $(shell date +%Y%m%d)
+SHA := $(shell git rev-parse --short HEAD)
+DIRTY := $(shell git diff --quiet && git diff --cached --quiet || echo "-dirty")
+TAG := v$(DATE)-$(SHA)${DIRTY}
+
 build:
-	docker build -t sleepymole/corplink:latest .
+	docker build -t $(REPO):latest .
+	docker tag $(REPO):latest $(REPO):$(TAG)
+
+push:
+	docker push $(REPO):latest
+	docker push $(REPO):$(TAG)
+
+run:
+	docker run -d \
+		--name=corplink \
+		--hostname=ubuntu \
+		--device=/dev/net/tun \
+		--cap-add=NET_ADMIN \
+		--shm-size=512m \
+		-p 6901:6901 \
+		-p 8888:8118 \
+		-p 1088:1080 \
+		-e VNC_PW=password \
+		$(REPO):$(TAG)
--- src/ubuntu/install/corplink/install_corplink.sh
+++ src/ubuntu/install/corplink/install_corplink.sh
@@ -10,7 +10,7 @@ echo 'listen-address 0.0.0.0:8118' >>/etc/privoxy/config
 mkdir -p /var/log/corplink/
 cat <<EOF >/etc/supervisor/conf.d/corplink.conf
 [program:corplink]
-command=/opt/Corplink/corplink-service
+command=/opt/apps/com.volcengine.feilian/files/corplink-service
 autostart=true
 autorestart=true
 stderr_logfile=/var/log/corplink/stderr.log
@@ -45,7 +45,7 @@ cat <<EOF >/usr/local/bin/fixdns.sh
 set -x
 while true; do
   sleep 5
-  dns=\$(jq -r '.DNS[0]' /opt/Corplink/vpn.conf  2>/dev/null)
+  dns=\$(jq -r '.DNS[0]' /opt/apps/com.volcengine.feilian/files/vpn.conf 2>/dev/null)
   [ -z "\$dns" ] && continue
   grep -q "\$dns" /etc/resolv.conf && continue
   echo "nameserver \${dns}" >/etc/resolv.conf
@@ -62,7 +62,7 @@ stderr_logfile=/var/log/fixdns/stderr.log
 stdout_logfile=/var/log/fixdns/stdout.log
 EOF
 
-wget -q -O corplink.deb https://oss-s3.ifeilian.com/linux/FeiLian_Linux_v2.0.9_r615_97b98b.deb
+wget -q -O corplink.deb https://cdn.isealsuite.com/linux/FeiLian_Linux_amd64_v3.0.23_r5833_c34f4b.deb
 apt install ./corplink.deb -y
 rm corplink.deb
 cp /usr/share/applications/corplink.desktop $HOME/Desktop/
